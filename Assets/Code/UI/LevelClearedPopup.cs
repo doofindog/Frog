03d1e2bf9ae4d269f93c1d1bbb9ff67c58459093
Mauno Kiiskinen <31925951+maukii@users.noreply.github.com>
2026-06-17 19:00:48 +0300

    Update level loading logic

--- Assets/Code/UI/LevelClearedPopup.cs
+++ Assets/Code/UI/LevelClearedPopup.cs
@@ -1,9 +1,18 @@
 using Febucci.TextAnimatorForUnity;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelClearedPopup : MonoBehaviour
 {
     [SerializeField] private TypewriterComponent typewriter;
+    [SerializeField] private LevelLoader levelLoader;
+    [SerializeField] private float punch = 0.3f;
+    [SerializeField] private float punchDuration = 0.4f;
+    [SerializeField] private int punchVibrato = 6;
+
+    private Vector3 _baseScale;
+
+    private void Awake() => _baseScale = typewriter.transform.localScale;
 
     private void Start() => typewriter.gameObject.SetActive(false);
 
@@ -11,6 +20,29 @@ public class LevelClearedPopup : MonoBehaviour
     public void Show()
     {
         typewriter.gameObject.SetActive(true);
+
+        typewriter.onTextShowed.RemoveListener(HandleTextShowed);
+        typewriter.onTextShowed.AddListener(HandleTextShowed);
+
         typewriter.StartShowingText(restart: true);
+    }
+
+    private void HandleTextShowed()
+    {
+        typewriter.onTextShowed.RemoveListener(HandleTextShowed);
+
+        var t = typewriter.transform;
+        t.DOKill();
+        t.localScale = _baseScale;
+        t.DOPunchScale(Vector3.one * punch, punchDuration, punchVibrato)
+            .OnComplete(GoToNextLevel);
+    }
+
+    private void GoToNextLevel()
+    {
+        typewriter.gameObject.SetActive(false);
+
+        if (levelLoader != null)
+            levelLoader.LoadNextLevel();
     }
 }
