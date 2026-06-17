849bf36511bb0734aa610fa63d24b287836e4958
Mauno Kiiskinen <31925951+maukii@users.noreply.github.com>
2026-06-17 17:55:38 +0300

    frog graphics and mood bubbles
    show mood bubbles when placement changes based on the mood

--- Assets/Code/UI/CustomerExpression.cs
+++ Assets/Code/UI/CustomerExpression.cs
@@ -1,34 +1,75 @@
 using UnityEngine;
+using DG.Tweening;
 
 public enum CustomerMood { Neutral, Happy, Unhappy }
 
 [RequireComponent(typeof(Draggable))]
 public class CustomerExpression : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer frogGraphic;
+    [SerializeField] private SpriteRenderer happyBubbleGraphic;
+    [SerializeField] private SpriteRenderer unhappyBubbleGraphic;
     [SerializeField] private Sprite neutral;
     [SerializeField] private Sprite happy;
     [SerializeField] private Sprite unhappy;
+    [SerializeField] private float bubbleScaleDuration = 0.3f;
 
     private Draggable _draggable;
+    private Vector3 _happyBubbleScale;
+    private Vector3 _unhappyBubbleScale;
+    private CustomerMood _currentMood = CustomerMood.Neutral;
 
     public string FrogName => _draggable.frogName;
 
     private void Awake()
     {
         _draggable = GetComponent<Draggable>();
-        SetMood(CustomerMood.Neutral);
+
+        _happyBubbleScale = happyBubbleGraphic.transform.localScale;
+        _unhappyBubbleScale = unhappyBubbleGraphic.transform.localScale;
+
+        happyBubbleGraphic.enabled = false;
+        unhappyBubbleGraphic.enabled = false;
+
+        if (frogGraphic != null)
+            frogGraphic.sprite = neutral;
     }
 
     public void SetMood(CustomerMood mood)
     {
-        if (spriteRenderer == null) return;
+        if (mood == _currentMood) return;
+        _currentMood = mood;
 
-        spriteRenderer.sprite = mood switch
+        if (frogGraphic != null)
         {
-            CustomerMood.Happy => happy,
-            CustomerMood.Unhappy => unhappy,
-            _ => neutral,
-        };
+            frogGraphic.sprite = mood switch
+            {
+                CustomerMood.Happy => happy,
+                CustomerMood.Unhappy => unhappy,
+                _ => neutral,
+            };
+        }
+
+        ShowBubble(happyBubbleGraphic, _happyBubbleScale, mood == CustomerMood.Happy);
+        ShowBubble(unhappyBubbleGraphic, _unhappyBubbleScale, mood == CustomerMood.Unhappy);
+    }
+
+    private void ShowBubble(SpriteRenderer bubble, Vector3 baseScale, bool show)
+    {
+        if (bubble == null) return;
+
+        bubble.transform.DOKill();
+
+        if (show)
+        {
+            bubble.enabled = true;
+            bubble.transform.localScale = Vector3.zero;
+            bubble.transform.DOScale(baseScale, bubbleScaleDuration).SetEase(Ease.OutBack);
+        }
+        else
+        {
+            bubble.enabled = false;
+            bubble.transform.localScale = baseScale;
+        }
     }
 }
